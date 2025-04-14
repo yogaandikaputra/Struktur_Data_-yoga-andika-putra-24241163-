a0d1a1f3b6a4348cf3b501e3089cb1a4219589e6
yoga andika putra-Alprog-e <andikaputrayoga139@gmail.com>
2025-04-14 17:00:46 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,26 @@
+# perbandingan kemungkinan nilai = true/salah (bolean)
+# 1 > 2 =
+# <, >, >=, <=, !=, not
+# a = 2 digit nim pertama
+# b = 2 digit nim terakhir
+
+a = 24
+b = 67
+hasil = a < b
+print(hasil)
+
+hasil = a > b
+print(hasil)
+
+hasil = a >= b
+print(hasil)
+
+hasil = a <= b
+print(hasil)
+
+hasil = a != b
+print(hasil)
+
+hasil = (" a not b ") 
+print(hasil)
+
